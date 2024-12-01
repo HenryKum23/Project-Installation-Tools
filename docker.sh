52c2c8b1a03a7c2e0343a72e4e23527fe50d76cf
Henry Kumah <142521180+HenryKum23@users.noreply.github.com>
2024-12-01 12:22:30 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,3 +5,7 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker $USER   #my case is ubuntu
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+
+
+Installing and running SonarQube as a container in docker 
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
